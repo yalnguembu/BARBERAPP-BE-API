0d43a9965cd6ba8bcc7d312950657cc827298bdb
yalnguembu <yalnguembu@gmail.com>
2023-03-18 01:32:14 +0100

    BARBERAPP-BE-5 -story:service crud

---
+++ src/modules/services/index.ts
@@ -0,0 +1,5 @@
+export * from "./serviceController";
+export * from "./serviceModel";
+export * from "./serviceServices";
+export * from "./dto";
+export * from "./Service";
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -20,3 +20,18 @@ export type CategorySchema = {
   title?: string;
   summary?: string;
 };
+
+export type ServiceSchema = {
+  _id?: string;
+  name: string;
+  description?: string;
+  category: string;
+  picture?: string;
+  price: number;
+  duration: number;
+};
+
+export type DecodedToken = {
+  _id: string;
+  email: string;
+};
