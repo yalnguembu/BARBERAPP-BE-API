100bc92b77a2a918e76e2c97cb4630f55aa830a6
yalnguembu <yalnguembu@gmail.com>
2023-03-28 11:10:45 +0100

    -enahancement:readat the router for permit user to login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { type Request, type Response, Router, NextFunction } from "express";
 import express from "express";
-import { join } from "path";
 
 import { AuthController } from "../modules/auth";
 import { UserController } from "../modules/users";
@@ -17,39 +16,67 @@ router.post("/auth/register", AuthController.register);
 router.post("/auth/verify-token", AuthController.verifyToken);
 
 router.use("/storage", express.static("public"));
-router.use(isUserConnected);
 
-router
-  .route("/user/:id")
-  .get(UserController.getById)
-  .put(UserController.update)
-  .delete(UserController.delete);
-router.get("/users", isUserAdmin, UserController.getAll);
+router.get("/user/:id", isUserConnected, UserController.getById);
+router.put("/user/:id", isUserConnected, UserController.update);
+router.delete("/user/:id", isUserConnected, UserController.delete);
+router.get("/users", isUserConnected, isUserAdmin, UserController.getAll);
 
-router.post("/category", CategoryController.create);
-router
-  .route("/category/:id")
-  .put(CategoryController.update)
-  .delete(CategoryController.delete);
-router.get("/categories", CategoryController.getAll);
+router.post(
+  "/category",
+  isUserConnected,
+  isUserAdmin,
+  CategoryController.create
+);
+router.put(
+  "/category/:id",
+  isUserConnected,
+  isUserAdmin,
+  CategoryController.update
+);
+router.delete(
+  "/category/:id",
+  isUserConnected,
+  isUserAdmin,
+  CategoryController.delete
+);
+router.get(
+  "/categories",
+  isUserConnected,
+  isUserAdmin,
+  CategoryController.getAll
+);
 
-router.post("/service", isUserAdmin, ServiceController.create);
-router
-  .route("/service/:id")
-  .get(ServiceController.getById)
-  .put(ServiceController.update)
-  .delete(ServiceController.delete);
+router.post("/service", isUserConnected, isUserAdmin, ServiceController.create);
+router.get("/service/:id", isUserConnected, ServiceController.getById);
+router.put(
+  "/service/:id",
+  isUserConnected,
+  isUserAdmin,
+  ServiceController.update
+);
+router.delete(
+  "/service/:id",
+  isUserConnected,
+  isUserAdmin,
+  ServiceController.delete
+);
 router.get("/services", isUserConnected, ServiceController.getAll);
 
-router
-  .post("/reservation", ReservationController.create)
-  .get("/reservations", isUserAdmin, ReservationController.getAll);
+router.post("/reservation", isUserConnected, ReservationController.create);
+router.get(
+  "/reservations",
+  isUserConnected,
+  ReservationController.getAll
+);
 
-router
-  .route("/reservation/:id")
-  .get(ReservationController.getById)
-  .put(ReservationController.update)
-  .delete(ReservationController.delete);
+router.put("/reservation/:id", isUserConnected, ReservationController.update);
+router.delete(
+  "/reservation/:id",
+  isUserConnected,
+  ReservationController.delete
+);
+router.get("/reservation/:id", isUserConnected, ReservationController.getById);
 
 router.use("*", (req: Request, res: Response, next: NextFunction) => {
   next(new NotFoundError(`'${req.baseUrl}'`));
